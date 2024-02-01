957938e5c90fd9b51b2f77a81f598dd8cbf52df0
Jesse <jessekaiser93@gmail.com>
2024-02-01 20:31:35 +0100

    Update url in conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ from conan.tools.cmake import CMakeToolchain, CMakeDeps
 class PieCutterConan(ConanFile):
     name = "piecutter"
     version = "1.0"
-    url = "https://github.com/Diesel993/conan-intro.git"
+    url = "https://github.com/jkaiser993/conan-intro.git"
     requires = "units/2.3.3"
     tool_requires = "cmake/3.28.1" 
     test_requires = "gtest/1.14.0"
