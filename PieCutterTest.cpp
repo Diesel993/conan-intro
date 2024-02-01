f0c30f2ec65200b2a662f759893a3a7e5790cb7c
Jesse <jessekaiser93@gmail.com>
2024-02-01 16:01:35 +0100

    Finished tests

--- PieCutterTest.cpp
+++ PieCutterTest.cpp
@@ -1,18 +1,24 @@
 #include "gtest/gtest.h"
+#include "PieCutter.hpp"
+#include <memory>
 
-
-class PieCutterTest : public Test
+class PieCutterTest : public testing::Test
 {
     public:
-        PieCutterTest()
-        {
+        PieCutterTest() = default;
 
-        }
-
-    
+    protected:
+        PieCutter piecutter{};
 };
 
-TEST_F(PieCutterTest, todo)
+TEST_F(PieCutterTest, WhenNeedTwelvePieces_TheReturnedAngleShouldBe30Deg)
 {
-    EXPECT_TRUE(true);
+    auto result = piecutter.getAnglePerPiece(12);
+    EXPECT_EQ(result, 30_deg);
+}
+
+TEST_F(PieCutterTest, WhenThePieIs200CmBy100mm_ThenTheAreaOfThePieIsXSqm)
+{
+    auto area = piecutter.getAreaOfPieInAnAwkwardWay(200_cm, 100_mm);
+    EXPECT_EQ(area, 0.2_sq_m);
 }
