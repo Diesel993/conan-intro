0a1d41e7fd71e3f239bbf85b8342eb4aeef03d46
Jesse <jessekaiser93@gmail.com>
2024-02-02 12:04:35 +0100

    Updated main for demo

--- main.cpp
+++ main.cpp
@@ -9,8 +9,15 @@ using namespace units::length;
 int main()
 {
     PieCutter piecutter{};
-    auto area = piecutter.getAreaOfPieInAnAwkwardWay(300_cm, 500_mm);
-    std::cout << "Area of this pie is: " << area << " square meters. \n";
+    
+    int pieces;
+    std::cout << "Provide the number of pieces you want: ";
+    std::cin >> pieces;
+    auto angle = piecutter.getAnglePerPiece(pieces);
+    std::cout << "Each piece needs to have an angle of: " << angle << "\n";
+
+    // auto area = piecutter.getAreaOfPieInAnAwkwardWay(300_cm, 500_mm);
+    // std::cout << "Area of this pie is: " << area << " \n";
     
     return 0;
 }
